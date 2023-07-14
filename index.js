998724c0b12e2e28e34a8c989db0f7ba39283ff9
fishboyraduk <rjayson@protonmail.com>
2023-07-14 08:25:00 +0300

    Inital Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const port = 3000;
+
+app.set('views', 'views');
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post('/store-bmi-result', (req, res)=>{
+    const bmiResult = {
+        height: req.body.height,
+        weight: req.body.weight,
+        bmi: req.body.bmi,
+    };
+    const jsonResult = JSON.stringify(bmiResult);
+    
+    fs.appendFile('bmi-results.json', jsonResult + '\n', (err)=>{
+        if(err){
+            console.error(err);
+            res.status(500).send('Error Storing BMI Result');
+        } else {
+            res.send('BMI result stored successfully');
+        }
+    });
+});
+app.get('/', function (request, response){
+    response.render('reports');
+});
+
+app.listen(port);
+console.log('server is listenning on port 3000');
